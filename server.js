55340a0698b5bb56035daf010b8500a306a7521f
sunbal <sunbalabro786@gmail.com>
2021-12-16 19:55:12 +0500

    pes  server

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ const cors = require('cors')
 const bcrypt = require('bcrypt')
 const myPlaintextPassword = 'nadir';
 const saltRounds = 10;
+const mysql = require('mysql')
+
 const {v4 : userId} = require('uuid')
 const {v4 : locationId} = require('uuid')
+
 app.use(bodyParser.json())
 app.use(cors())
 
@@ -15,16 +18,44 @@ app.get('/', (req, res) => {
     res.send("Server is working")
 })
 
+///////// connecting to mysql database  /////////
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'testing_server'
+});
+
+connection.connect((err)=>{
+   if(err){
+       console.log("err " + err)
+   }
+});
+
+
+// const usersDetail = connection.query('SELECT * FROM `user` WHERE 1',(error,result,fields)=>{
+//     if (error) throw error
+//     console.log("result : " + JSON.stringify(result))
+    
+// })
+
+
 //// create user ////
 
 app.post('/createuser', (req, res) => {
     const useruniqueid = userId()
-    try {
-        const { email, password, fullName, type ,} = req.body
+    try {   
+        const { email, password, fullName, type , loggedIn} = req.body
         bcrypt.genSalt(saltRounds,(err,salt)=>{
             bcrypt.hash(password,salt,(err,hash)=>{
-                const userdata = { email, password: hash, fullName, type , useruniqueid }
-                return res.json({ success: true, message: "successfully createed user ", userdata })
+                const userdata = { email, password: hash, fullName, type , useruniqueid,loggedIn}
+                const values = [[ email , hash , fullName , type ,useruniqueid,loggedIn]]
+                const query = `INSERT INTO user (email , password , fullName , type , userId , loggedIn) VALUES ?`
+                connection.query(query,[values],(err,result)=>{
+                    if (err) res.json({ success: false, message: "something went wrong in creating user" })
+                    return res.json({ success: true, message: "successfully createed user ", userdata })
+                })
             })
         })
     } catch(err) {
@@ -39,9 +70,14 @@ app.post('/createuser', (req, res) => {
 
 app.post('/edituser', (req, res) => {
     try {
-        const { fullName } = req.body
-        const edituserdata = { fullName }
-        res.json({ success: true, message: "successfully edit user ", edituserdata })
+        const { fullName , userId} = req.body
+        const newName = fullName
+        const user = userId 
+        const query = `UPDATE user set fullName = ? WHERE userId = ?`
+        connection.query(query,[newName,user],(err,results)=>{
+            if(err) res.json({ success: false, message: "something went wrong in adding user" }) 
+          return res.json({ success: true, message: "successfully edit user " })
+        })
     }
     catch(err) {
         if (err) {
@@ -56,13 +92,34 @@ app.post('/edituser', (req, res) => {
 app.post('/deleteuser',(req,res)=>{
     try{
         const {userId} = req.body
-        const deleteUser = {userId}
-        res.json({success: true , message:"successfully deleted user" , deleteUser})
+        
+        const deleteUser = userId
+        const query = `DELETE FROM user WHERE userId = ?`
+        connection.query(query,deleteUser,(err,results)=>{
+            if(err) res.json({success: false, message:"something went wrong in deleting user"}) 
+           return res.json({success: true , message:"successfully deleted user" , deleteUser})
+        })
     }catch(err){
         if(err){
             res.json({success: false, message:"something went wrong in deleting user"})
             console.log(err)
         }
+    }
+})
+
+///// get all users /////
+
+app.get('/getallusers',(req,res)=>{
+    try{
+        connection.query('SELECT * FROM `user` WHERE 1',(error,result,fields)=>{
+                if (error) throw error
+                const users = result
+                res.json({success: true , message: "successfully get all users" , users})
+            })
+    }catch(err){
+       if(err){
+           res.json({success: false , message: "somenthing went wrong in getting all users"})
+       }
     }
 })
 
